9926d304eff697b050c876f61dcc5cbe5a419978
Adam Craig <lateralusajc@gmail.com>
2016-06-17 12:02:26 -0700

    Modify displayStars helper to remove empty star glyphicons.

--- app/helpers/star-rating.js
+++ app/helpers/star-rating.js
@@ -5,43 +5,33 @@ export function starRating(params) {
 
   if(stars === 5) {
     return Ember.String.htmlSafe(
-      '<b class="glyphicon glyphicon-star"></b>' +
-      '<b class="glyphicon glyphicon-star"></b>' +
-      '<b class="glyphicon glyphicon-star"></b>' +
-      '<b class="glyphicon glyphicon-star"></b>' +
-      '<b class="glyphicon glyphicon-star"></b>'
+      '<b class="glyphicon glyphicon-flash"></b>' +
+      '<b class="glyphicon glyphicon-flash"></b>' +
+      '<b class="glyphicon glyphicon-flash"></b>' +
+      '<b class="glyphicon glyphicon-flash"></b>' +
+      '<b class="glyphicon glyphicon-flash"></b>'
     );
   } else if (stars === 4) {
     return Ember.String.htmlSafe(
-      '<b class="glyphicon glyphicon-star"></b>' +
-      '<b class="glyphicon glyphicon-star"></b>' +
-      '<b class="glyphicon glyphicon-star"></b>' +
-      '<b class="glyphicon glyphicon-star"></b>' +
-      '<b class="glyphicon glyphicon-star-empty"></b>'
+      '<b class="glyphicon glyphicon-flash"></b>' +
+      '<b class="glyphicon glyphicon-flash"></b>' +
+      '<b class="glyphicon glyphicon-flash"></b>' +
+      '<b class="glyphicon glyphicon-flash"></b>'
     );
   } else if (stars === 3) {
     return Ember.String.htmlSafe(
-      '<b class="glyphicon glyphicon-star"></b>' +
-      '<b class="glyphicon glyphicon-star"></b>' +
-      '<b class="glyphicon glyphicon-star"></b>' +
-      '<b class="glyphicon glyphicon-star-empty"></b>' +
-      '<b class="glyphicon glyphicon-star-empty"></b>'
+      '<b class="glyphicon glyphicon-flash"></b>' +
+      '<b class="glyphicon glyphicon-flash"></b>' +
+      '<b class="glyphicon glyphicon-flash"></b>'
     );
   } else if (stars === 2) {
     return Ember.String.htmlSafe(
-      '<b class="glyphicon glyphicon-star"></b>' +
-      '<b class="glyphicon glyphicon-star"></b>' +
-      '<b class="glyphicon glyphicon-star-empty"></b>' +
-      '<b class="glyphicon glyphicon-star-empty"></b>' +
-      '<b class="glyphicon glyphicon-star-empty"></b>'
+      '<b class="glyphicon glyphicon-flash"></b>' +
+      '<b class="glyphicon glyphicon-flash"></b>'
     );
   } else if (stars === 1) {
     return Ember.String.htmlSafe(
-      '<b class="glyphicon glyphicon-star"></b>' +
-      '<b class="glyphicon glyphicon-star-empty"></b>' +
-      '<b class="glyphicon glyphicon-star-empty"></b>' +
-      '<b class="glyphicon glyphicon-star-empty"></b>' +
-      '<b class="glyphicon glyphicon-star-empty"></b>'
+      '<b class="glyphicon glyphicon-flash"></b>'
     );
   } else {
     return Ember.String.htmlSafe(
